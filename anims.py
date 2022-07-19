da044826f76dfe279498cac27b6b515981f96d18
vaclavrozhon <vaclavrozhon@gmail.com>
2022-07-19 11:04:24 +0200

    first commit

---
+++ anims.py
@@ -0,0 +1,9 @@
+import random
+from manim import *
+
+
+class SimpleCounting(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
